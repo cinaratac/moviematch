5592b955abeb04ad4799d0de235ab216b6d6858b
cinaratac <2309111176@stu.istinye.edu.tr>
2025-12-20 12:07:50 +0300

    site yapıulıd

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,23 +1,10 @@
-import java.util.Properties
-import java.io.FileInputStream
-import java.io.File
 plugins {
     id("com.android.application")
-    // START: FlutterFire Configuration
     id("com.google.gms.google-services")
-    // END: FlutterFire Configuration
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    
-    
 }
 
-val keystoreProperties = Properties()
-val keystorePropertiesFile = rootProject.file("key.properties")
-if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-}
 android {
     namespace = "com.kozmosoft.cinematch"
     compileSdk = flutter.compileSdkVersion
@@ -34,10 +21,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.kozmosoft.cinematch"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -46,21 +30,21 @@ android {
 
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String?
-            keyPassword = keystoreProperties["keyPassword"] as String?
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String?
+            // ARTIK DOSYA OKUMUYORUZ, DİREKT YAZDIK:
+            keyAlias = "my_alias"
+            keyPassword = "12neymar12"
+            storePassword = "12neymar12"
+            
+            // Bu dosyanın android/app klasöründe olması şart:
+            storeFile = file("my_new_app_key.jks") 
         }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-           signingConfig = signingConfigs.getByName("release")
-
-           isMinifyEnabled = true
-            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false 
+            isShrinkResources = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
